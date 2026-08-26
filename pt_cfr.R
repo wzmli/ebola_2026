66090ee4532c9b756e85b8547c1966d094d8a9be
wzmli <wzmli@DESKTOP-RQDLLDH.localdomain>
2026-08-26 08:17:30 -0400

    Autocommit

--- pt_cfr.R
+++ pt_cfr.R
@@ -6,11 +6,18 @@ dat <- (rdsRead()
 		, province = as.factor(province)
 		, cfr = deaths/cases
 	)
+	|> group_by(province)
+	|> mutate(total_cases = max(cases,na.rm=TRUE))
 )
 
+print(dat)
+
+
 gg <- (ggplot(dat,aes(date,cfr,color=province))
-	+ geom_point()
+	+ geom_point(aes(size=total_cases))
+	+ scale_size_area()
 	+ geom_line()
+#	+ facet_wrap(~province)
 )
 
 print(gg)
