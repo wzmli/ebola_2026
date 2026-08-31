081a8ea10545107c048b94e1289cb244f3d202fd
wzmli <wzmichael.li@gmail.com>
2026-08-31 11:57:20 -0400

    naive Rt

--- ptplot.R
+++ ptplot.R
@@ -1,6 +1,7 @@
 library(tidyverse);theme_set(theme_bw())
 library(zoo)
 library(shellpipes)
+startGraphics(width=5, height=3)
 
 dat <- (csvRead()
 	|> arrange(province,date)
@@ -12,6 +13,10 @@ dat <- (csvRead()
 		, new_deaths = diff(c(0,deaths))
 		, avgcases = round(rollmean(new_cases,k=7,fill=NA,align="right",na.rm=TRUE))
 		, avgdeath = round(rollmean(new_deaths,k=7,fill=NA,align="right",na.rm=TRUE))
+		, avgcases2 = round(rollmean(avgcases,k=7,fill=NA,align="right",na.rm=TRUE))
+		, avgdeath2 = round(rollmean(avgdeath,k=7,fill=NA,align="right",na.rm=TRUE))
+		, naiveRt_cases = avgcases/avgcases2
+		, naiveRt2 = rollmean(naiveRt_cases,k=7,fill=NA,align="right",na.rm=TRUE)
 	)
 )
 
@@ -25,5 +30,18 @@ gg <- (ggplot(filter(dat,new_cases>0, province %in% c("Ituri","Nord-Kivu","Haut-
 print(dat,n=Inf)
 
 print(gg)
+
+ggRt <- (ggplot(filter(dat,new_cases>0, province %in% c("Ituri","Nord-Kivu","Haut-Uele")), aes(date))
+#	+ geom_point(aes(y=new_cases,color=province))
+	+ geom_smooth(aes(y=naiveRt_cases,color=province,fill=province))
+#	+ geom_line(aes(y=naiveRt2,color=province,fill=province))
+	+ geom_hline(aes(yintercept = 1))
+#	+ geom_smooth()
+	+ scale_color_manual(values=c("red","blue","green"))
+	+ scale_fill_manual(values=c("red","blue","green"))
+#	+ facet_wrap(~province, scale="free", nrow=3)
+)
+
+print(ggRt)
 
 rdsSave(dat)
